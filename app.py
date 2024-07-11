cb760e5aa56bc38d54372d0ccf8f3313a8b3fdd3
yaminshweyihtay <yaminshweyihtay@gmail.com>
2024-07-11 01:19:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
-
-
-@app.route('/')
 def index():
     return render_template('index.html')
 
